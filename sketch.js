3ed2d82828ba41f941bad720bbb417cd4bd410f9
Devam360 <73469709+Devam360@users.noreply.github.com>
2021-05-03 12:42:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var bullet, wall;
+
+var speed, weight, dam;
+
+function setup() {
+  createCanvas(1600,400);
+
+  speed = random(223, 321);
+  weight = random(30, 52);
+  
+  thickness = random(22, 83);
+
+  bullet=createSprite(50, 200, 100, 10);
+  bullet.velocityX = speed
+  bullet.shapeColor = color(0);
+
+
+  wall = createSprite(1500, 200, thickness, height/2)
+
+  
+  dam = (0.5*weight*speed*speed)/thickness*thickness*thickness
+
+}
+
+function draw() {
+  background(255,255,255);
+  if(bullet.x < wall.x + wall.width &&
+    bullet.x + bullet.width > wall.x &&
+    bullet.y < wall.y + wall.height &&
+    bullet.y + bullet.height > wall.y){
+      bullet.velocityX = 0
+      if(dam<10){
+        wall.shapeColor="green"
+      }
+      else{
+        wall.shapeColor = "red"
+      }
+    }
+  drawSprites();
+}
